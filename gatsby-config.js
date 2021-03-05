5ef2db4369e5773f7fc4144a537e4e6f0468427b
Saranga7 <saranga.boo@gmail.com>
2021-03-05 12:46:32 +0530

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Portfolio website of Saranga Kingkor Mahanta',
     siteUrl: 'https://sarangamahanta.netlify.app', // No trailing slash allowed!
-    image: '/Slogo.png', // Path to your image you placed in the 'static' folder
+    image: '/logo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@mahanta_saranga',
   },
   plugins: [
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SarangaMahanta',
+        short_name: 'SarangaMahanta',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/Slogo.png',
+        icon: 'src/images/logo.png',
       },
     },
     `gatsby-plugin-offline`,
